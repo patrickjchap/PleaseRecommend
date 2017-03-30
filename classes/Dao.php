ffc0e38dbf6747f877f921a7e7dcfccecc5dcebc
Patrick Chapman <Patrick Chapman>
2017-03-30 14:39:49 -0600

    Added dao class

---
+++ classes/Dao.php
@@ -0,0 +1,70 @@
+<?php
+
+	class Dao {
+		private $host = "localhost";
+		private $db = "mysql";
+		private $user = "root";
+		private $password = "";
+		
+		public function getConnection() {
+			try{
+				$conn = new PDO("mysql:host={$this->host};
+				dbname={$this->db}", $this->user,
+				$this->password);
+			} catch (Exception $e) {
+				echo "database connection error";
+				exit;
+			}
+			return $conn;
+		}
+		
+		public function createUser($newUser, $newPassword, $newEmail,
+		$newFName, $newLName) {
+			$conn = $this->getConnection();
+			$q = $conn->prepare("INSERT INTO pr_user (USER_EMAIL, USER_FNAME,
+			USER_LNAME, USER_NAME, USER_PASSWORD) VALUES ('$newEmail',
+			'$newFName', '$newLName', '$newUser', '$newPassword')");
+	
+			return $conn->query($q);
+		}
+		
+		public function getNumUser($qUsername) {
+			$conn = $this->getConnection();
+			$q = $conn->prepare("SELECT COUNT(*) FROM pr_user 
+			WHERE USER_NAME = '$qUsername'");
+			
+			$q->execute();
+			$rowCount = $q->fetchColumn(0);
+			
+			return $rowCount;
+			
+		}
+		
+		public function getNumEmail($qEmail) {
+			$conn = $this->getConnection();
+			$q = $conn->prepare("SELECT COUNT(*) FROM pr_user 
+			WHERE USER_EMAIL = '$qEmail'");
+			
+			$q->execute();
+			$rowCount = $q->fetchColumn(0);
+			
+			return $rowCount;
+			
+		}
+		
+		public function getNumUserWithPass($qUsername, $qPassword){
+			$conn = $this->getConnection();
+			$q = $conn->prepare("SELECT COUNT(*) FROM pr_user WHERE
+			USER_NAME = '$qUsername' and USER_PASSWORD = '$qPassword'");
+			
+			$q->execute();
+			$rowCount = $q->fetchColumn(0);
+			
+			return $rowCount;
+			
+			
+		}
+		
+	}
+
+?>
